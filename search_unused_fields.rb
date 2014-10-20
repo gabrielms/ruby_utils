3117d3295328ef1ad0f21b13991e31edc3dbfe02
gabrielms <gabrielmatosdesouza@gmail.com>
2014-10-20 17:47:14 -0200

    Initial commit;

---
+++ search_unused_fields.rb
@@ -0,0 +1,67 @@
+#encoding:utf-8
+
+#TODO pegar a linha que possui o as e descobrir o nome do campo
+#pegar o nome dos fields usados que possuam o #P{campo}
+puts "Digite o nome do arquivo"
+
+diretorio_relatorios = "/home/gabriel/projects/ieducar/ieducar-utils/reports/jasperreports/"
+
+nome_arquivo = gets.chomp
+
+
+f = File.open(diretorio_relatorios + nome_arquivo, 'r')
+
+campos_sql = []
+
+f.each do |f|
+  if f.match(/\b#{"as "}\b/i)
+    campo = ""
+    index = f.upcase.index("AS ")
+    index += 3
+    for index in index..f.length - 1
+      campo += f[index]
+    end
+    campo.chomp!
+    campo.sub!(/,/, "")
+    campos_sql << campo
+  end
+end
+
+campos_ireport = []
+
+f = File.open(diretorio_relatorios + nome_arquivo, 'r')
+
+f.each do |linha|
+   campo_ireport = []
+   campos = linha.scan(/\$F\{[\w]*\}/)
+   campos.each do |campo|
+    campo.sub! "$F{", ""
+    campo.sub! "}", ""
+    campo.sub! "[", ""
+    campo.sub! "]", ""
+    campo_ireport << campo
+   end
+
+   campos_ireport << campo_ireport
+    # campo = ""
+    # index = f.upcase.index("$F{")
+    # index += 3
+    # for index in index..f.length - 1
+    #    break if f[index] == "}"
+    #    campo += f[index]
+    # end
+    
+end
+
+campos_ireport.flatten!
+campos_ireport.reject! { |c| c.empty? }
+
+puts campos_sql.inspect
+
+puts campos_ireport.inspect
+
+puts "Campos que podem ser removidos do SQL: "
+
+campos_sql.each do |campo|
+  puts campo unless campos_ireport.include?(campo)
+end
